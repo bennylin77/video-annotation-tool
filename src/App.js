a88cc3b70a88637cd38280712d8cccf7e1084c60
Benny Lin <bennylin77@gmail.com>
2018-04-23 21:45:40 +0000

    update

--- src/App.js
+++ src/App.js
@@ -82,7 +82,56 @@ class App extends Component {
 	}
 	handleVideoProgress = state => {
 		if (!this.state.seeking) {
-    	this.setState({played: state.played})
+    	//this.setState({played: state.played})
+			const played = state.played
+			//const target = e.target
+			this.setState((prevState, props) => {
+				return { played: played, objects: prevState.objects.map( obj =>{
+						let offsetX = obj.offset.x
+						let offsetY = obj.offset.y
+						let trajectories = obj.trajectories
+						for( let i = 0; i < trajectories.length; i++){
+							if(played >= trajectories[i].time){
+								if(i!==trajectories.length-1 && played >= trajectories[i+1].time)
+									continue;
+								let x, y;
+
+								if(i===trajectories.length-1){
+									offsetX = trajectories[i].x - trajectories[0].x;
+									offsetY = trajectories[i].y - trajectories[0].y;
+								}else{
+									let startTraj = trajectories[i], endTraj = trajectories[i+1]
+									let lapseTime = endTraj.time - startTraj.time;
+									let xSlope = (endTraj.x - startTraj.x)/lapseTime, ySlope = (endTraj.y - startTraj.y)/lapseTime;
+									// set x and y position
+									let curTime = played - startTraj.time;
+									x = xSlope * curTime + startTraj.x;
+									y = ySlope * curTime + startTraj.y;
+									offsetX = x - trajectories[0].x;
+									offsetY = y - trajectories[0].y;
+									//obj.Group.x(offsetX)
+									//obj.Group.y(offsetY)
+								}
+								obj.Group.x(offsetX)
+								obj.Group.y(offsetY)
+								break;
+							}
+						}
+						//console.log(offsetX, offsetY);
+						return { ...obj,
+										 offset: {x:offsetX, y:offsetY }
+									 };
+					})
+				}
+			})
+
+
+
+
+
+
+
+
 		}
   }
   handleVideoDuration = duration => {
